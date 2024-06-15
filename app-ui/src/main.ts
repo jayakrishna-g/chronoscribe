405ae87f0cc06638162efc5b6f3f810fa2bc802c
Jayakrishna Goud Gandhamalla <gandhamallajayakrishna@hotmail.com>
2024-06-15 19:07:30 -0400

    Migrated Bootstrapping code using ng generate @angular/core:standalone

--- app-ui/src/main.ts
+++ app-ui/src/main.ts
@@ -1,13 +1,25 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './app/shared/shared.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AuthenticationService } from './app/core/authentication/authentication.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, SharedModule.forRoot(), ToastrModule.forRoot()),
+        AuthenticationService,
+        provideAnimations(),
+    ]
+})
   .catch((err) => console.error(err));
